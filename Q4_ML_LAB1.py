36540579ebd78db9d9061059a8f6c8d6af50116e
FrancisKammattil <francisshabu.k@gmail.com>
2026-07-12 18:56:59 +0530

    Add files via upload

--- Q4_ML_LAB1.py
+++ Q4_ML_LAB1.py
@@ -1,13 +1,12 @@
-s = input("Enter the string: ")
-max_char = s[0]
-max_count = 0
-for i in s:
-    count = 0
-    for j in s:
-        if i == j:
-            count += 1
-    if count > max_count:
-        max_count = count
-        max_char = i
-print("Highest occurring character:", max_char)
-print("Occurrence count:", max_count)
+#def matrix(m):
+    
+n=int(input("Enter the row number"))
+m=int(input("Enter the column number"))
+if(n==m):
+    x=int(input("Power of matrix"))
+    arry=[[x for i in range(n)]for j in range(m)]
+            
+
+
+
+
