be7eb50563423265d62d89332a218eee5008ef25
FrancisKammattil <francisshabu.k@gmail.com>
2026-07-12 15:28:01 +0530

    Update Q5_ML_LAB1.py

--- Q5_ML_LAB1.py
+++ Q5_ML_LAB1.py
@@ -1,17 +1,17 @@
 import random
 import math
 
-m2 = [random.randint(1, 10) for i in range(25)]
-length = len(m2)
-mean = sum(m2) / length
+m2=[random.randint(1, 10) for i in range(25)]
+length=len(m2)
+mean = sum(m2)/length
 m2.sort()
-if length % 2 == 1:  
-    median = m2[length//2]
-    print("The median is:", median)
+if length%2==1:  
+    median=m2[length//2]
+    print("The median is:",median)
 else:  
     median1=m2[(length//2)-1]
     median2=m2[length//2]
     print("There are two medians:",median1,median2)
-print("Mean:", mean)
-print("Sorted List:", m2)
+print("Mean:",mean)
+print("Sorted List:",m2)
 
